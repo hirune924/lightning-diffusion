8c1b57ff28eed3114ab6d8a06a26c5133902a2ae
hirune924 <nori.o920924@outlook.jp>
2024-05-14 18:49:27 +0900

    update

--- scripts/main.py
+++ scripts/main.py
@@ -2,7 +2,8 @@ from lightning.pytorch.cli import LightningCLI
 # simple demo classes for your convenience
 from lightning_diffusion.data.datasets import HFDataModule
 from lightning_diffusion.model.stable_diffusion import StableDiffusionModel
-
+import torch
+torch.set_float32_matmul_precision('medium')
 def cli_main():
     cli = LightningCLI(StableDiffusionModel, HFDataModule, subclass_mode_data=False, subclass_mode_model=False,
                        parser_kwargs={"parser_mode": "omegaconf"})
--- src/lightning_diffusion/model/stable_diffusion.py
+++ src/lightning_diffusion/model/stable_diffusion.py
@@ -4,10 +4,16 @@ import torch.nn.functional as F
 from diffusers import AutoencoderKL, DDPMScheduler, UNet2DConditionModel, StableDiffusionPipeline
 from transformers import CLIPTextModel, CLIPTokenizer
 import numpy as np
+from peft import get_peft_model, LoraConfig
 
 class StableDiffusionModel(L.LightningModule):
-    def __init__(self, base_model: str = "runwayml/stable-diffusion-v1-5"):
+    def __init__(self, 
+                 base_model: str = "runwayml/stable-diffusion-v1-5", 
+                 train_mode: str = "unet_attn",
+                 gradient_checkpointing: bool = False,
+                 input_perturbation_gamma: float = 0.0):
         super().__init__()
+        self.input_perturbation_gamma = input_perturbation_gamma
         self.tokenizer = CLIPTokenizer.from_pretrained(pretrained_model_name_or_path=base_model,
                                                        subfolder="tokenizer")
         self.scheduler = DDPMScheduler.from_pretrained(pretrained_model_name_or_path=base_model,
@@ -22,10 +28,31 @@ class StableDiffusionModel(L.LightningModule):
         self.vae.requires_grad_(False)
         self.text_encoder.requires_grad_(False)
         self.unet.requires_grad_(False)
-        
-        for name, param in self.unet.named_parameters():
-            if 'to_k' in name or 'to_v' in name:
-                param.requires_grad = True
+
+        if train_mode == "unet_all":
+            self.unet.requires_grad_(True)
+
+        elif train_mode == "unet_attn":
+            for name, param in self.unet.named_parameters():
+                if "to_k" in name or "to_v" in name:
+                    param.requires_grad = True
+
+        elif train_mode == "unet_lora":
+            lora_config = LoraConfig(r=8,
+                                     lora_alpha=8,
+                                     target_modules=["to_q", "to_v", "to_k", "to_out.0"])
+            self.unet = get_peft_model(self.unet, lora_config)
+            self.unet.print_trainable_parameters()
+
+        elif train_mode == "text_encoder_lora":
+            lora_config = LoraConfig(r=8,
+                                     lora_alpha=8,
+                                     target_modules=["q_proj", "k_proj", "v_proj", "out_proj"])
+            self.text_encoder = get_peft_model(self.text_encoder, lora_config)
+            self.text_encoder.print_trainable_parameters()
+
+        if gradient_checkpointing:
+            self.unet.enable_gradient_checkpointing()
 
     def configure_optimizers(self):
         optimizer = torch.optim.AdamW(self.parameters(), lr=1e-4, weight_decay=1e-2)
@@ -83,7 +110,11 @@ class StableDiffusionModel(L.LightningModule):
             0,self.scheduler.config.num_train_timesteps, (num_batches, ),
             dtype=torch.int64, device=self.device)
 
-        noisy_latents = self.scheduler.add_noise(latents, noise, timesteps)
+        if self.input_perturbation_gamma > 0:
+            noise = noise + self.input_perturbation_gamma * torch.randn_like(noise)
+            noisy_latents = self.scheduler.add_noise(latents, noise, timesteps)
+        else:
+            noisy_latents = self.scheduler.add_noise(latents, noise, timesteps)
 
         encoder_hidden_states = self.text_encoder(batch["text"], return_dict=True).last_hidden_state
 
@@ -92,7 +123,15 @@ class StableDiffusionModel(L.LightningModule):
             timesteps,
             encoder_hidden_states=encoder_hidden_states).sample
         
-        loss = F.mse_loss(noise_pred.float(), noise.float(), reduction="mean")
+        if self.scheduler.config.prediction_type == "epsilon":
+            gt = noise
+        elif self.scheduler.config.prediction_type == "v_prediction":
+            gt = self.scheduler.get_velocity(latents, noise, timesteps)
+        else:
+            msg = f"Unknown prediction type {self.scheduler.config.prediction_type}"
+            raise ValueError(msg)
+        
+        loss = F.mse_loss(noise_pred.float(), gt.float(), reduction="mean")
         self.log("train_loss", loss)
         return loss
         
